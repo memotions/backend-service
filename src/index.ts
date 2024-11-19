a6fbb7aa0688aa6f1bd27804a38a4e4c0a1c959e
Bayu Rizky Kurnia Pratama <bayurkp194@gmail.com>
2024-11-19 11:34:59 +0800

    feat: add logger and http logger

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-for (let i = 0; i < 10; i += 1) {
-  console.log('Hello, Memothians!');
-}
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import Logger from './utils/logger';
+import httpLogger from './middlewares/httpLogger';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(httpLogger);
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Halo, Memothians!',
+  });
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  Logger.info(`Server is running on port ${port}`);
+});
