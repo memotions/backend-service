f0d20ff7dcbbc987daa026c3a3cf02700aea5844
Bayu Rizky Kurnia Pratama <bayurkp194@gmail.com>
2024-11-25 17:18:04 +0800

    feat: api error class

---
+++ src/utils/apiError.ts
@@ -0,0 +1,19 @@
+export default class ApiError extends Error {
+  code: string;
+  statusCode: number;
+  details?: { [key: string]: any };
+
+  constructor(
+    code: string,
+    statusCode: number,
+    message: string,
+    details?: { [key: string]: any },
+  ) {
+    super(message);
+    this.code = code;
+    this.statusCode = statusCode;
+    this.details = details;
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
