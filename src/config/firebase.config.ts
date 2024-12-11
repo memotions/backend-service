e0e998a5d0d0de0dfb1de1d3249da3ff657ac7b1
Bayu Rizky Kurnia Pratama <bayurkp194@gmail.com>
2024-12-11 15:04:41 +0800

    fix: path service account

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,12 +1,13 @@
 import 'dotenv/config';
 import admin from 'firebase-admin';
+import path from 'path';
 
 const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT || '';
 
 const initializeFirebaseApp = () => {
   if (!admin.apps.length) {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert(path.resolve(serviceAccount)),
     });
   }
   return admin;
