393d2c3aa3a21f619e9825360065fc88053271b6
Bayu Rizky Kurnia Pratama <bayurkp194@gmail.com>
2024-12-11 15:20:06 +0800

    fix: firebase account

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,16 +1,34 @@
 import 'dotenv/config';
 import admin from 'firebase-admin';
+import fs from 'fs';
 import path from 'path';
+import Logger from '../utils/logger';
 
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT || '';
+const serviceAccountUrl = process.env.FIREBASE_SERVICE_ACCOUNT_URL || '';
 
-const initializeFirebaseApp = () => {
+const initializeFirebaseApp = async () => {
   if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert(path.resolve(serviceAccount)),
-    });
+    try {
+      const response = await fetch(serviceAccountUrl);
+      const buffer = await response.arrayBuffer();
+
+      const tempFilePath = path.resolve(
+        __dirname,
+        'firebaseServiceAccount.json',
+      );
+      fs.writeFileSync(tempFilePath, Buffer.from(buffer));
+
+      admin.initializeApp({
+        credential: admin.credential.cert(tempFilePath),
+      });
+
+      fs.unlinkSync(tempFilePath);
+
+      Logger.info('Firebase initialized successfully');
+    } catch (error) {
+      Logger.error('Failed to download or initialize Firebase:', error);
+    }
   }
-  return admin;
 };
 
 export default initializeFirebaseApp;
